36c4714d3196e4ef5874f5bb825b79e95175281a
seif <seif.m05@gmail.com>
2024-12-02 20:15:12 +0200

    General Files

--- CustomerAccounts.aspx.cs
+++ CustomerAccounts.aspx.cs
@@ -6,44 +6,24 @@ using System.Linq;
 using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using static Telecom_Web_App.Helpers;
 
 namespace Telecom_Web_App
 {
     public partial class CustomerAccounts : System.Web.UI.Page
     {
-        private string connectionString = WebConfigurationManager.ConnectionStrings["dbConnection"].ToString(); // Replace with your actual connection string
-
+        
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
-                DataTable customerData = GetCustomerData();
-                Session["GridData"] = customerData;
+                DataTable data = GetData("SELECT * FROM customer_profile");
+                Session["GridData"] = data;
                 Response.Redirect("/Result.aspx");
 
             }
         }
 
-        private DataTable GetCustomerData()
-        {
-            DataTable customerData = new DataTable();
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                string sql = "SELECT * FROM CustomerWallet"; // Use the complete view name
-
-                using (SqlCommand command = new SqlCommand(sql, connection))
-                {
-                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
-                    {
-                        adapter.Fill(customerData);
-                    }
-                }
-            }
-
-            return customerData;
-        }
+        
     }
 }
---
+++ Generals.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using System;
+using System.Web.Configuration;
+namespace Telecom_Web_App
+{
+    public class Helpers
+    {
+        public static DataTable GetData(String sqlLine)
+        {
+            DataTable customerData = new DataTable();
+            string connStr = WebConfigurationManager.ConnectionStrings["dbConnection"].ToString();
+
+            using (SqlConnection connection = new SqlConnection(connStr))
+            {
+                connection.Open();
+
+                string sql = sqlLine; // Use the complete view name
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(customerData);
+                    }
+                }
+            }
+
+            return customerData;
+        }
+    }
+}
+
+
